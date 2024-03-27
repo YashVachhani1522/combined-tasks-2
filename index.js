90d709ccb242cad0de4dc439b61d344f66645f07
yashpatel1522 <yashspatel3113@gmail.com>
2024-03-27 17:50:33 +0530

    student-attendance  Added..

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const cucu_cube = require('./routes/cucu-cube/cucu-cube');
 const tic_tac_toe = require('./routes/tic-tac-toe/tic-tac-toe');
 const sorting_Int = require('./routes/sorting-int-char-string/sorting');
 const jsevent = require('./routes/event-prac/event');
+const getatt = require('./routes/student-attendance/student');
 
 require('dotenv').config()
 
@@ -35,6 +36,7 @@ app.listen(process.env.PORT,(err)=>{
         app.use("/tic-tac-toe",tic_tac_toe)
         app.use("/sorting-int",sorting_Int)
         app.use("/js-event",jsevent)
+        app.use("/std-attendance",getatt)
         app.get("/",getLogin);
         console.log(`server listen on http://${process.env.HOST}:${process.env.PORT}`)
     }
