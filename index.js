658b023fe99689f052e3fe314577fba5ee315230
yashpatel1522 <yashspatel3113@gmail.com>
2024-03-27 15:32:48 +0530

    dynemic-table task Added..

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express=require('express');
 const path = require('path');
 const router = require('./routes/main-login-project/practical-1');
 const app=express();
-var cookieParser = require('cookie-parser')
+var cookieParser = require('cookie-parser');
+const dynemic_table = require('./routes/dynemic-table/dynemictable');
+const { getLogin } = require('./controllers/main-login-project/practical1');
 
 require('dotenv').config()
 
@@ -24,8 +26,8 @@ app.listen(process.env.PORT,(err)=>{
     else
     {   
         app.use("/main-login-project",router);
-
-        // app.use(router);
+        app.use("/dynemic-table",dynemic_table)
+        app.get("/",getLogin);
         console.log(`server listen on http://${process.env.HOST}:${process.env.PORT}`)
     }
 })
